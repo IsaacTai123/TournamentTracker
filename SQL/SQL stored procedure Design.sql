3505dea316768c72c23bc06f0e4bb90d7a08ecc4
isaac <kevincurry438@gmail.com>
2021-01-26 17:35:17 +0800

    SQL Connection

--- SQL/SQL stored procedure Design.sql
+++ SQL/SQL stored procedure Design.sql
@@ -10,3 +10,23 @@ begin
 	on t.TeamId = m.id
 	where TournamentId = @TournamentId;
 end
+
+
+CREATE PROCEDURE dbo.spPrizes_Insert
+	@PlaceNumber int,
+	@PlaceName nvarchar(50),
+	@PrizeAmount money,
+	@PrizePercentage float,
+	@id int = 0 output
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    INSERT INTO dbo.Prizes (PlaceNumber, PlaceName, PrizeAmount, PrizePercentage)
+	VALUES (@PlaceNumber, @PlaceName, @PrizeAmount, @PrizePercentage);
+
+	select @id = SCOPE_IDENTITY(); -- 把最新輸入的Id拿出來, the last identity which is the new Id we just insert into.
+END
+GO
--- TrackerLibrary/GlobalConfig.cs
+++ TrackerLibrary/GlobalConfig.cs
@@ -1,28 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Text;
 using TrackerLibrary.DataAccess;
+
 
 namespace TrackerLibrary
 {
     public static class GlobalConfig
     {
-        public static List<IDataConnection> Connections { get; private set; } = new List<IDataConnection>();
-        public static void InitializeConnections(bool database, bool textFiles)
+        public static IDataConnection Connection { get; private set; }
+
+        public static void InitializeConnections(DatabaseType db)
         {
-            if (database) // or you can use database == true, if the value is boolean we can just use (database)
+
+            if (db == DatabaseType.Sql) // or you can use database == true, if the value is boolean we can just use (database)
             {
                 // TODO - Set up the Sql connector properly
                 SqlConnector sql = new SqlConnector();
-                Connections.Add(sql);
+                Connection = sql;
             }
 
-            if (textFiles)
+            else if (db == DatabaseType.TextFile)
             {
                 // TODO - Create the Text Connection
                 TextConnector text = new TextConnector();
-                Connections.Add(text);
+                Connection = text;
             }
+        }
+
+        public static string CnnString(string name)
+        {
+            var output = ConfigurationManager.ConnectionStrings[name].ConnectionString;
+            return output;
         }
     }
 }
